1cc8d758192f628449e6d7de16c0946792a7aabd
Ada Clark <hiddenkrypt@gmail.com>
2024-12-16 12:29:03 -0800

    refactoring global names and moving setup function to the tools object

--- src/avd_tools.js
+++ src/avd_tools.js
@@ -1,6 +1,74 @@
 export function avd_tools() {
   return { 
-    tokenizeScript: tokenizeScript
+    tokenizeScript: tokenizeScript,
+    initialState: initialState,
+    setup: setup
+  }
+  
+  function setup(){
+    let avd_container = document.getElementById("avd_container")
+    if( avd_container ){
+      avd_container.innerHTML = '';
+    } else {
+      avd_container = document.createElement("div");
+      avd_container.id = "avd_container";    
+    }
+    
+    let element_list = {
+      avdContainer: document.createElement("div"),
+      avatarLeft: document.createElement("img"),
+      avatarRight: document.createElement("img"),
+      titleLeft: document.createElement("div"),
+      titleRight: document.createElement("div"),
+      dialogContainer: document.createElement("div")
+    };
+    for (let e in element_list){
+      element_list[e].id = e
+      avd_container.appendChild(element_list[e]);
+    }
+    element_list.dialog = document.createElement("div");
+    element_list.dialog.id = "dialog";
+    element_list.dialogContainer.appendChild(element_list.dialog);
+    function imageFallback() {
+      console.error(`Visual Dialog: Image not found (${this.src})`);
+      this.src = '/modules/visual-dialog/assets/missing_image.png';
+      this.onerror = null;
+    }
+    element_list.avatarLeft.onerror = imageFallback;
+    element_list.avatarRight.onerror = imageFallback;
+    document.body.appendChild(avd_container);
+    return element_list;
+  }
+  function initialState(){
+    return {
+      dialogDisplay: false,
+      left: null,
+      right: null,
+      scriptPointer: null,
+      script: null,
+      last: null,
+      narrator: {fontSpeech:"caviar_dreams, sans-serif"},
+      find: function(name){
+        if(this.left && (this.left.name == name || this.left.alias == name)){
+          return this.left;
+        }
+        if(this.right && (this.right.name == name || this.right.alias == name)){
+          return this.right;
+        }
+        if( name == 'narrator' ){
+          return this.narrator;
+        }
+        return {error:true};
+      },
+      side: function(name){
+        if(this.find(name) == this.left){
+          return "left";
+        } else if(this.find(name) == this.right){
+          return "right";
+        }
+        return "none";
+      }
+    };
   }
   function tokenizeScript( txt ){ 
     txt = txt.replace( /^\n+$/g, '' );
--- src/avd_visualDialog.js
+++ src/avd_visualDialog.js
@@ -2,98 +2,37 @@ import {avd_animations} from "./avd_animations.js";
 import {avd_tools} from "./avd_tools.js";
 Hooks.once('init', async function () {
   console.log('Visual Dialog | Initializing Visual Dialog');
-  window.avd_VisualDialog = new VisualDialog();
-  function initialState(){
-    return {
-      dialogDisplay: false,
-      left: null,
-      right: null,
-      scriptPointer: null,
-      script: null,
-      last: null,
-      narrator: {fontSpeech:"caviar_dreams, sans-serif"},
-      find: function(name){
-        if(this.left && (this.left.name == name || this.left.alias == name)){
-          return this.left;
-        }
-        if(this.right && (this.right.name == name || this.right.alias == name)){
-          return this.right;
-        }
-        if( name == 'narrator' ){
-          return this.narrator;
-        }
-        return {error:true};
-      },
-      side: function(name){
-        if(this.find(name) == this.left){
-          return "left";
-        } else if(this.find(name) == this.right){
-          return "right";
-        }
-        return "none";
-      }
-    } 
-  }
+  window.VisualDialog = new VisualDialog();
+
   function VisualDialog() {
     let animations = avd_animations();
     let tools = avd_tools();
-    let avd_state = initialState();
-    let avd_elements;
-    let avd_container = document.createElement("div");
-    avd_container.id = "avd_container";
-    avd_setup();
-    
-    function avd_setup(){
-      for (let [e,v] in avd_elements){
-        v.parentNode.removeChild(v);
-      }
-      avd_elements = {
-        avdContainer: document.createElement("div"),
-        avatarLeft: document.createElement("img"),
-        avatarRight: document.createElement("img"),
-        titleLeft: document.createElement("div"),
-        titleRight: document.createElement("div"),
-        dialogContainer: document.createElement("div")
-      };
-      for (let e in avd_elements){
-        avd_elements[e].id = e
-        avd_container.appendChild(avd_elements[e]);
-      }
-      avd_elements.dialog = document.createElement("div");
-      avd_elements.dialog.id = "dialog";
-      avd_elements.dialogContainer.appendChild(avd_elements.dialog);
-      function imageFallback() {
-        console.error(`Visual Dialog: Image not found (${this.src})`);
-        this.src = '/modules/visual-dialog/assets/missing_image.png';
-        this.onerror = null;
-      }
-      avd_elements.avatarLeft.onerror = imageFallback;
-      avd_elements.avatarRight.onerror = imageFallback;
-      document.body.appendChild(avd_container);
-    }
-    
+    let avd_state = tools.initialState();
+    let elements = tools.setup();
+    let script = "";
+    let scriptPointer = 1;
     
     this.loadScript = function( txt ){
-      avd_state.script = tools.tokenizeScript( txt );
-      avd_state.scriptPointer = -1;
+      script = tools.tokenizeScript( txt );
+      scriptPointer = -1;
     }
     var step = this.step = function(){
-      if( !avd_state.script ){
+      if( !script ){
         return;
       }
-      if(avd_state.scriptPointer >= avd_state.script.length-1 ){
+      if(scriptPointer >= script.length-1 ){
         endDialog()
         return;
       }
-      let line = avd_state.script[++avd_state.scriptPointer];
+      let line = script[++scriptPointer];
       readLine(line);
       
     };
     this.unstep = function(){
-      if(!avd_state.script || avd_state.scriptPointer <= 0 ){
+      if(!script || scriptPointer <= 0 ){
         return;
       }
-      let line = avd_state.script[--avd_state.scriptPointer]
+      let line = script[--scriptPointer]
       if( line.type =="speech" || line.type == "narration" ){
         readLine(line);
       } else {
@@ -111,7 +50,6 @@ Hooks.once('init', async function () {
           step();
         },
         "fontname": function(line){
-          console.log("set fontName: "+line.font);
           avd_state.find(line.name).fontName = line.font;
           setTitleFont(avd_state.side(line.name), line.font);
           step();
@@ -150,10 +88,10 @@ Hooks.once('init', async function () {
     
     function setTitleFont(side, font){
       if(side=="left"){
-        avd_elements.titleLeft.style.fontFamily = font;
+        elements.titleLeft.style.fontFamily = font;
       }
       if(side=="right"){
-        avd_elements.titleRight.style.fontFamily = font;
+        elements.titleRight.style.fontFamily = font;
       }
     }
     
@@ -164,8 +102,8 @@ Hooks.once('init', async function () {
         side:side,
         state: "enter"
       };
-      let enterAvatar = (side=="left") ? avd_elements.avatarLeft: avd_elements.avatarRight;
-      let enterTitle = (side=="left") ? avd_elements.titleLeft: avd_elements.titleRight;
+      let enterAvatar = (side=="left") ? elements.avatarLeft: elements.avatarRight;
+      let enterTitle = (side=="left") ? elements.titleLeft: elements.titleRight;
       
       enterTitle.innerHTML = characterName;
       enterAvatar.src = characterImage;
@@ -191,102 +129,100 @@ Hooks.once('init', async function () {
     function showDialog(){
       avd_state.dialogDisplay = true;
       let anim = animations.textBox.show;
-      avd_elements.dialogContainer.animate(anim.a, anim.t);
+      elements.dialogContainer.animate(anim.a, anim.t);
     }
     function sayDialog( side, dialogText ){
-      console.log("say:-"+side);
-      console.log(avd_state[side]);
       if( !avd_state.dialogDisplay ){
         showDialog();
       }
       let wrapL='“'
       let wrapR='”'
       if(side=="left"){
-        if(avd_state.left.fontSpeech){avd_elements.dialog.style.fontFamily = avd_state.left.fontSpeech;}
-        avd_elements.dialog.style.backgroundcolor = "rgba(0,0,0,.6)";
-        avd_elements.dialog.style.fontSize = "1.9vw";
-        avd_elements.dialog.style.textAlign = "left";
-        avd_elements.dialog.style.fontStyle = "normal";
+        if(avd_state.left.fontSpeech){elements.dialog.style.fontFamily = avd_state.left.fontSpeech;}
+        elements.dialog.style.backgroundcolor = "rgba(0,0,0,.6)";
+        elements.dialog.style.fontSize = "1.9vw";
+        elements.dialog.style.textAlign = "left";
+        elements.dialog.style.fontStyle = "normal";
         let anim = animations.avatarRight.gray;
-        avd_elements.avatarRight.animate(anim.a, anim.t)
+        elements.avatarRight.animate(anim.a, anim.t)
         anim = animations.avatarLeft.degray;
-        avd_elements.avatarLeft.animate(anim.a, anim.t)
+        elements.avatarLeft.animate(anim.a, anim.t)
         anim = animations.textBox.left;
-        avd_elements.dialogContainer.animate(anim.a, anim.t);
+        elements.dialogContainer.animate(anim.a, anim.t);
         anim = animations.textBox.right;
-        avd_elements.dialog.animate(anim.a, anim.t);
+        elements.dialog.animate(anim.a, anim.t);
       }
       if(side=="right"){
         if(avd_state.right.fontSpeech){
-          avd_elements.dialog.style.fontFamily = avd_state.right.fontSpeech;
+          elements.dialog.style.fontFamily = avd_state.right.fontSpeech;
         }
-        avd_elements.dialog.style.backgroundcolor = "rgba(0,0,0,.6)";
-        avd_elements.dialog.style.fontSize = "1.9vw";
-        avd_elements.dialog.style.textAlign = "right";
-        avd_elements.dialog.style.fontStyle = "normal";
+        elements.dialog.style.backgroundcolor = "rgba(0,0,0,.6)";
+        elements.dialog.style.fontSize = "1.9vw";
+        elements.dialog.style.textAlign = "right";
+        elements.dialog.style.fontStyle = "normal";
         let anim = animations.avatarLeft.gray;
-        avd_elements.avatarLeft.animate(anim.a, anim.t)
+        elements.avatarLeft.animate(anim.a, anim.t)
         anim = animations.avatarRight.degray;
-        avd_elements.avatarRight.animate(anim.a, anim.t)
+        elements.avatarRight.animate(anim.a, anim.t)
         anim = animations.textBox.right;
-        avd_elements.dialogContainer.animate(anim.a, anim.t);
+        elements.dialogContainer.animate(anim.a, anim.t);
         anim = animations.textBox.left;
-        avd_elements.dialog.animate(anim.a, anim.t);
+        elements.dialog.animate(anim.a, anim.t);
       }
       if(side=="narrator"){
-        if(avd_state.narrator.fontSpeech){avd_elements.dialog.style.fontFamily = avd_state.narrator.fontSpeech;}
-        avd_elements.dialog.style.backgroundcolor = "rgba(0,0,0,.9)";
-        avd_elements.dialog.style.fontSize = "1.6vw";
-        avd_elements.dialog.style.textAlign = "center";
-        avd_elements.dialog.style.fontStyle = "italic";
+        if(avd_state.narrator.fontSpeech){elements.dialog.style.fontFamily = avd_state.narrator.fontSpeech;}
+        elements.dialog.style.backgroundcolor = "rgba(0,0,0,.9)";
+        elements.dialog.style.fontSize = "1.6vw";
+        elements.dialog.style.textAlign = "center";
+        elements.dialog.style.fontStyle = "italic";
         let anim = animations.avatarLeft.gray;
-        avd_elements.avatarLeft.animate(anim.a, anim.t)
+        elements.avatarLeft.animate(anim.a, anim.t)
         anim = animations.avatarRight.gray;
-        avd_elements.avatarRight.animate(anim.a, anim.t)
+        elements.avatarRight.animate(anim.a, anim.t)
         wrapR = "";
         wrapL = "";
         anim = animations.textBox.center;
-        avd_elements.dialogContainer.animate(anim.a, anim.t);
-        avd_elements.dialog.animate(anim.a, anim.t);
+        elements.dialogContainer.animate(anim.a, anim.t);
+        elements.dialog.animate(anim.a, anim.t);
       }
       
       let anim = animations.dialog.show;
-      avd_elements.dialog.animate(anim.a, anim.t);
-      avd_elements.dialog.innerHTML = `${wrapL}${dialogText}${wrapR}`;
+      elements.dialog.animate(anim.a, anim.t);
+      elements.dialog.innerHTML = `${wrapL}${dialogText}${wrapR}`;
     }
     function exitCharacter( name ){
       if(avd_state.left && name == avd_state.left.alias){
         let anim = animations.avatarLeft.exit;
-        avd_elements.avatarLeft.animate(anim.a, anim.t);
+        elements.avatarLeft.animate(anim.a, anim.t);
         anim = animations.titleLeft.exit;
-        avd_elements.titleLeft.animate(anim.a, anim.t);
+        elements.titleLeft.animate(anim.a, anim.t);
       } else if(avd_state.right && name == avd_state.right.alias) {
         let anim = animations.avatarRight.exit;
-        avd_elements.avatarRight.animate(anim.a, anim.t);
+        elements.avatarRight.animate(anim.a, anim.t);
         anim = vd_animations.titleRight.exit;
-        avd_elements.titleRight.animate(anim.a, anim.t);
+        elements.titleRight.animate(anim.a, anim.t);
       }       
     };
     function endDialog(){
       if( avd_state.left ){
         let anim = animations.avatarLeft.exit;
-        avd_elements.avatarLeft.animate(anim.a, anim.t);
+        elements.avatarLeft.animate(anim.a, anim.t);
         anim = animations.titleLeft.exit;
-        avd_elements.titleLeft.animate(anim.a, anim.t);
+        elements.titleLeft.animate(anim.a, anim.t);
       }
       if( avd_state.right ){
         let anim = animations.avatarRight.exit;
-        avd_elements.avatarRight.animate(anim.a, anim.t);
+        elements.avatarRight.animate(anim.a, anim.t);
         anim = animations.titleRight.exit;
-        avd_elements.titleRight.animate(anim.a, anim.t);
+        elements.titleRight.animate(anim.a, anim.t);
       }
       if( avd_state.dialogDisplay ){
         let anim = animations.dialog.exit;
-        avd_elements.dialog.animate(anim.a, anim.t);
+        elements.dialog.animate(anim.a, anim.t);
         anim = animations.textBox.exit;
-        avd_elements.dialogContainer.animate(anim.a, anim.t);
+        elements.dialogContainer.animate(anim.a, anim.t);
       }
-      avd_state = initialState();
+      avd_state = tools.initialState();
     }
   }
 
